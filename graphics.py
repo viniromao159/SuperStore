b301850ae0361426a329865a42b16c86aee68ddb
Vinicius L. Romão <vini.romao159@gmail.com>
2025-05-24 10:58:31 -0300

    Adjuste barplot (Bar plot - Top 10 sales of product)

--- graphics.py
+++ graphics.py
@@ -2,11 +2,12 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 df = pd.read_csv("Data/SuperStore - Data.csv")
 df.head()
 
-#Converter sales para float
+#Sales to float
 df['Sales'] = (
     df['Sales']
     .astype(str)
@@ -18,22 +19,64 @@ df['Sales'] = (
 )
 
 # %%
+#Bar plot - Top sales per Sub-category
+
 subCategory = (df.groupby(['Sub-Category'])['Order ID'].count()).sort_values(ascending=False).reset_index()
-plot = sns.barplot(x='Order ID',y='Sub-Category', data=subCategory)
-plot.set_title('Top sales per Sub-category')
-plot.set_xlabel('Sales count')
+
+colors = plt.cm.Blues(subCategory['Order ID'])
+
+sns.barplot(x='Order ID',y='Sub-Category', data=subCategory, width=0.7, palette=colors, zorder=3)
+
+plt.title('Top sales per Sub-category', pad=15)
+
+plt.xlabel('Sales count', labelpad=15)
+
+plt.ylabel('Sub-Category', labelpad=15)
+
+plt.grid(axis='x', alpha=0.3, zorder=0)
+
+sns.despine()
 
 # %%
-region = (df.groupby(['Region'])['Order ID'].count()).reset_index()
-plot = sns.barplot(y='Order ID',x='Region', data=region)
-plot.set_title('Top sales per Region')
-plot.set_xlabel('Sales count')
+#Line plot - Sales history per month
+
+df["Ship fdate"] = pd.to_datetime(df['Ship Date'], format='%d/%m/%Y')
+
+df["Ship fdate"] = df["Ship fdate"].dt.to_period('M').dt.to_timestamp(how='end')
+
+SalesMonth = (
+    df.groupby('Ship fdate')['Sales']
+    .sum()
+    .reset_index()
+    .sort_values(by='Ship fdate')
+)
+
+plt.figure(figsize=(9,6))
+
+sns.lineplot(y='Sales', x='Ship fdate', data=SalesMonth)
+
+plt.title('Sales per date')
+
+plt.xlabel('Sales value', labelpad=15)
+
+plt.xticks(rotation=45)
 
 # %%
+#Bar plot - Top 10 sales of product
+
 product = (df.groupby(['Product Name'])['Sales'].sum()).nlargest(10).reset_index()
-plot = sns.barplot(x='Sales', y='Product Name', data=product)
-plot.set_title('Top sales per Product Name')
-plot.set_xlabel('Sales count')
+
+sns.barplot(x='Sales', y='Product Name', data=product, width=0.7, zorder=3)
+
+plt.title('Top 10 sales of product', pad=15)
+
+plt.xlabel('Sales count', labelpad=15)
+
+plt.ylabel('Sub-Category', labelpad=15)
+
+plt.grid(axis='x', alpha=0.3, zorder=0)
+
+sns.despine()
 
 # %%
 City = (df.groupby(['City'])['Order ID'].count()).nlargest(10).reset_index()
@@ -54,29 +97,7 @@ plot.set_title('Top sales per Segment')
 plot.set_xlabel('Sales count')
 
 # %%
-df["Ship month-year"] = pd.to_datetime(df['Ship Date'], format='%d/%m/%Y')
-
-df["Ship month-year"] = sorted(df["Ship month-year"])
-
-df["Ship month-year"] = df["Ship month-year"].dt.strftime('%m/%Y')
-
-df["Ship month-year"].reset_index()
-
-df_data = df[["Ship month-year",'Sales']] 
-
-df_data = df_data.sort_values(by="Ship month-year")
-
-SalesMonth = df_data.groupby(by='Ship month-year', group_keys=True)[['Sales']].sum()
-
-SalesMonth
-
-# plt.figure(figsize=(10,4))
-
-# sns.lineplot(y='Sales', x='Ship month-year', data=SalesMonth)
-
-# plt.title('Sales per date')
-
-# plt.xlabel('Sales value', labelpad=15)
-
-# plt.xticks(rotation=45)
-# %%
+region = (df.groupby(['Region'])['Order ID'].count()).reset_index()
+plot = sns.barplot(y='Order ID',x='Region', data=region)
+plot.set_title('Top sales per Region')
+plot.set_xlabel('Sales count')
